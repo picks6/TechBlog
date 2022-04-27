da8cad7ef1adee0acd51c115a2f72272cb47831f
Zach P <95437073+picks6@users.noreply.github.com>
2022-04-27 11:22:22 -0400

    updated models

---
+++ models/Blog.js
@@ -0,0 +1,29 @@
+// Blod should be the model for blogposts
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize
+  });
+
+
+module.exports = Blog;
---
+++ models/index.js
@@ -0,0 +1,36 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+  });
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'cascade'
+  });
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id',
+  onDelete: 'cascade'
+});
+
+
+
+
+module.exports = { User, Blog, Comment};
